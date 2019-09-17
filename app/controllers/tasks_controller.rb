a1c2d5eededd2d8f79329ac2cdd2dea1729c0844
ryuki.abe <r.a.nowitzki41@softbank.ne.jp>
2019-09-17 05:32:01 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task  = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task が正常にされました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+end
+
+private
+
+  def set_task
+    @task = task.find(params[:id])
+  end
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
