89f352b697a1c6ce73a4a5ab4442c6b7c565f653
ryuki.abe <r.a.nowitzki41@softbank.ne.jp>
2019-09-17 07:02:09 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,9 +48,9 @@ end
 private
 
   def set_task
-    @task = task.find(params[:id])
+    @task = Task.find(params[:id])
   end
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_104649) do
+ActiveRecord::Schema.define(version: 2019_09_17_060235) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
