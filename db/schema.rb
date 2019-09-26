238161a4d8d78c3a3643991efda3e0bcaf02d6ff
ryuki.abe <r.a.nowitzki41@softbank.ne.jp>
2019-09-26 04:19:47 +0000

    tasklist login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_060235) do
+ActiveRecord::Schema.define(version: 2019_09_22_141711) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "status"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
